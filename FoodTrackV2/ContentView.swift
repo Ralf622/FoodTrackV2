79b5f93d317a2e36c0ac164772032453a4077fe0
Morel <morelraphael2@gmail.com>
2025-11-07 00:36:02 +0100

    Add main page

--- FoodTrackV2/ContentView.swift
+++ FoodTrackV2/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Text("This is the main page ")
+                .navigationTitle("FoodTrack")
+            
+            
         }
-        .padding()
+        
     }
+    
 }
 
 #Preview {
