9c7200f60711d88d3a56c783cb3e0afbdc3d3bc0
Morel <morelraphael2@gmail.com>
2025-11-06 23:57:01 +0100

    Adding initial source code

---
+++ FoodTrackV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodTrackV2
+//
+//  Created by Raphael Morel on 06/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodTrackV2/FoodTrackV2App.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackV2App.swift
+//  FoodTrackV2
+//
+//  Created by Raphael Morel on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
